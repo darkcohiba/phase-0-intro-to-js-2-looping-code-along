b437acfdf1869425e69a9d4c1863255049cbab18
Sam <samuel.waters@gmail.com>
2022-02-01 10:44:19 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(guests, event){
+    for (let i = 0; i < guests.length; i++) {
+        messages.push(`Thank you, ${guests[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+
+    return messages;
+}
+
+writeCards(guests, "Birthday")
+
+
+function countDown (x) {
+    while (x >= 0) {
+    console.log(x);
+    x--;
+    }
+}
